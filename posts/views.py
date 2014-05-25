4501774ea5e02d920bb7f3d14fa84b09b0d2c788
Radames Santiago <radamessantiago180@gmail.com>
2014-05-25 18:39:51 -0300

    criando buscar e Destaques 1/2

--- posts/views.py
+++ posts/views.py
@@ -1,24 +1,45 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response
-# from django.template import RequestContext
+from django.template import RequestContext
 from posts.models import Post
 from datetime import datetime
 from django.utils.timezone import utc
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 def index(request):
-    list_post = Post.objects.all().order_by('-data_criacao')
-    paginator = Paginator(list_post, 1)
+    if request.method == 'GET':
     
-    try:
-        page = int(request.GET.get('page', '1'))
-    except ValueError:
-        page = 1
+        list_post = Post.objects.all().order_by('-data_criacao')
+        paginator = Paginator(list_post, 1)
         
-    try:
-        posts = paginator.page(page)
-    except (EmptyPage, InvalidPage):
-        posts = paginator.page(paginator.num_pages)        
+        try:
+            page = int(request.GET.get('page', '1'))
+        except ValueError:
+            page = 1
+            
+        try:
+            posts = paginator.page(page)
+        except (EmptyPage, InvalidPage):
+            posts = paginator.page(paginator.num_pages)        
+            
+        return render_to_response('posts/index.html', {"list_post": posts})  
+         
+    elif request.method == 'POST':
+
+        list_post = Post.objects.get(corpo_icontains=request.POST['busca']).order_by('-data_criacao')
+        paginator = Paginator(list_post, 1)
         
-    return render_to_response('posts/index.html', {"list_post": posts})                  
+        try:
+            page = int(request.GET.get('page', '1'))
+        except ValueError:
+            page = 1
+            
+        try:
+            posts = paginator.page(page)
+        except (EmptyPage, InvalidPage):
+            posts = paginator.page(paginator.num_pages)        
+            
+        return render_to_response('posts/index.html', {"list_post": posts})                       
 
